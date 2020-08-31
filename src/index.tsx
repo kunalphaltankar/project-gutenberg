57f615a9df391cc68c59b79acc5e1e331598fbb6
kunal phaltankar <kunalphaltankar@gmail.com>
2020-08-31 22:04:45 +0530

    Montserrat fonts added

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export default function Home(props: any) {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <Text style={{fontFamily: 'Montserrat-Italic'}}>Hi Kunal</Text>
+    </View>
+  );
+}
